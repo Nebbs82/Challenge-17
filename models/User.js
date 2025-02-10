bfe44974704ec8cc0e938cc14d260063439ff69f
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-02-10 17:37:04 -0600

    Creating the models ..I think

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username:{
+        type: String,
+        required: true,
+        unique: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        match: [/\S+@\S+\.\S+/, 'Please enter a valid email address']
+    },
+    thoughts: [
+        {
+          type: Schema.Types.ObjectId,
+          ref: 'Thought'
+        }
+    ],
+    friends: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+        }
+    ]
+}, {timestamps: true});
+ 
+userSchemaV.virtual('friendCount').get(function() {
+    return this.frineds.length;
+});
+
+const User = mongoose.model('User', userSchema);
+module.exports = User;
